758b8a9a085b4527b07e1b9d6bbae6f67c805348
stalksprout <connolly.samuel@googlemail.com>
2020-07-17 15:36:23 +0100

    Minor tidy up, add some comments
    remove some unneeded vars. garbage.

--- deathsounds.js
+++ deathsounds.js
@@ -1,14 +1,14 @@
-function getTrackIds() {
+//If we've got a folder named deathsounds go grab us a random track id
+function getRandomDeathTrackIds() {
     var jbFolder = JSON.parse(Campaign().get('jukeboxfolder'))
     var trackId = ""
-    var indice = 0
-    for(x in jbFolder) {
-        log(jbFolder[x])
-        if (jbFolder[x]["n"] == "deathsounds") {
-            log("deathsounds")
-            var indice = randomInteger(jbFolder[x]["i"].length)
-            tracks = jbFolder[x]["i"]
-            trackId = tracks[indice - 1]
+    for(playlist in jbFolder) {
+	//Playlist[n] is the name of the folder in the jukebox
+        if (jbFolder[playlist]["n"] == "deathsounds") {
+	    //Get random song between 1 and length
+            var indice = randomInteger(jbFolder[playlist]["i"].length)
+            //i is the index with meaningful stuff in honestly, track IDs
+            trackId = jbFolder[playlist]["i"][indice - 1]
         }
     }
     return trackId
@@ -17,9 +17,13 @@ function getTrackIds() {
 
 
 on("destroy:graphic", function(obj) {
+    // We don't want to trigger these on background image destructions.
+    // imagine how awful editing maps would be jesus
     if(obj.get("layer") == "objects") {
-    	var jbtrack = getObj('jukeboxtrack', getTrackIds())
-    	jbtrack.set({playing:false,softstop:false,volume:70});
+    	var jbtrack = getObj('jukeboxtrack', getRandomDeathTrackIds())
+	//Don't know if this is the right way to do this? I seem to get double sounds
+	//On first sound played
+    	jbtrack.set({playing:false,softstop:true,volume:70});
     	jbtrack.set({playing:true,softstop:false,volume:70});
     }
 });
